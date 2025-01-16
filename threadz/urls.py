17e37205495617a63d52589e33675a8b71f14eae
NickG35 <ngeorge356@gmail.com>
2025-01-16 14:06:13 -0500

    fixed app urls

--- threadz/urls.py
+++ threadz/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('products/', include('products.urls')),
-    path('orders/', include('orders.urls')),
+    path('', include('users.urls')),
+    path('', include('products.urls')),
+    path('', include('orders.urls')),
 ]
