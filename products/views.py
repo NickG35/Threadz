72c6ab3133a63fabbb69897563ef47fced5ccec5
NickG35 <ngeorge356@gmail.com>
2025-01-20 06:56:37 -0500

    fetching api products to pass to index page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,19 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    #fetch api url data
+    api_url = 'https://fakestoreapi.com/products'
+    try:
+        response = requests.get(api_url)
+        products = response.json()
+    #if there is a fetch error, return an empty list of products with error
+    except requests.exceptions.RequestException as e:
+        print(f"API fetch error: {e}")
+        products = []
+
+    #pass fetched products to index page
+    return render(request, 'index.html', {
+        'products': products
+    })
