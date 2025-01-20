89a564a89db4cb53531ae631d3716bb6b5f2d47a
NickG35 <ngeorge356@gmail.com>
2025-01-20 07:43:08 -0500

    fetching clothing api to populate home page

--- products/views.py
+++ products/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #fetch api url data
-    api_url = 'https://fakestoreapi.com/products'
     try:
-        response = requests.get(api_url)
+        url = "https://asos2.p.rapidapi.com/categories/list"
+        querystring = {"country":"US","lang":"en-US"}
+        headers = {
+            "x-rapidapi-key": "f6875f623cmsh6831be6ced70d87p192f91jsnaf20a3d3d5e4",
+            "x-rapidapi-host": "asos2.p.rapidapi.com"
+        }
+        response = requests.get(url, headers=headers, params=querystring)
         products = response.json()
+
     #if there is a fetch error, return an empty list of products with error
     except requests.exceptions.RequestException as e:
         print(f"API fetch error: {e}")
