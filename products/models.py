9697a1d3b6149d62a30c2b19b44de79291f22981
NickG35 <ngeorge356@gmail.com>
2025-01-22 10:02:13 -0500

    created a product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    image = models.ImageField(upload_to='product_images/', null=False)
+    price = models.IntegerField(null=False)
+    description = models.CharField(max_length=300, null=False)
+    category = models.CharField(max_length=100, null=False)
