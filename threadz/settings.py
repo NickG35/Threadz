2fd4b7d43bab9e3b58db70d2480aa0ae30bbb0c9
NickG35 <ngeorge356@gmail.com>
2025-01-22 10:06:41 -0500

    joined media with base_dir to store image uploads

--- threadz/settings.py
+++ threadz/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+
+MEDIA_URL = '/media/' #url path to access media files
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where media files are
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
