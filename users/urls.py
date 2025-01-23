a357bae2f75803dd9ff1feaa05da638131bd8540
NickG35 <ngeorge356@gmail.com>
2025-01-23 13:03:25 -0500

    added authentication urls

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile", views.profile, name="profile"),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path("profile/", views.profile, name="profile"),
 ]
