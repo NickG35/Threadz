7eaa89af439793388389e4bf7fa98fdfefd45c00
NickG35 <ngeorge356@gmail.com>
2025-01-23 13:04:03 -0500

    implemented register, logout, and login functionalities

--- users/views.py
+++ users/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index').url
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'registration/login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm
+        return render(request, 'registration/register.html', {
+            'form': form,
+        })
+
 def profile(request):
     return render(request, 'profile.html')
