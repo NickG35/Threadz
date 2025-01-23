60c1811f1f1af224749a007607ae27fbd17552c9
NickG35 <ngeorge356@gmail.com>
2025-01-23 14:34:01 -0500

    changed logout redirect and adjusted registration implementation with error handling

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.db import IntegrityError
+from .models import User
 
 # Create your views here.
 
@@ -13,7 +13,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index').url
+            return redirect('index')
         else:
             return redirect('login')
     else:
@@ -21,23 +21,35 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('login')
+    return redirect('index')
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('index')
-    else:
-        form = UserCreationForm
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirmation = request.POST.get("confirmation")
+        errors = {}
+
+        if not username:
+            errors['username'] = "Please enter a username."
+        if not password:
+            errors['password'] = "Please enter a password."
+        if password != confirmation:
+            errors['confirmation'] = "Passwords do not match."
+        if not errors:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                errors['username'] = "Username already taken."
+    
         return render(request, 'registration/register.html', {
-            'form': form,
+            'errors': errors
         })
+    
+    return render(request, "registration/register.html")
 
 def profile(request):
     return render(request, 'profile.html')
