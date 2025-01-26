4d5099607750bbd7bc926e7daa79241e33c12315
NickG35 <ngeorge356@gmail.com>
2025-01-26 07:21:33 -0500

    changed url names for auth links so theres no conflict with built in system

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='user_login'),
+    path('logout/', views.logout_user, name='user_logout'),
     path('register/', views.register_user, name='register'),
     path("profile/", views.profile, name="profile"),
 ]
