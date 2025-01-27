11e45be9c3dcb56788aeb634fdc725f66e82dc41
NickG35 <ngeorge356@gmail.com>
2025-01-27 07:18:12 -0500

    altered profile url to make it user specific

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.login_user, name='user_login'),
     path('logout/', views.logout_user, name='user_logout'),
     path('register/', views.register_user, name='register'),
-    path("profile/", views.profile, name="profile"),
+    path("profile/<int:profile_id>", views.profile, name="profile"),
 ]
