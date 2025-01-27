e62887759ff17dc8d7fd7f509496f4ac7b9e6dc3
NickG35 <ngeorge356@gmail.com>
2025-01-27 07:52:35 -0500

    added product creation url

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("product/<int:product_id>", views.product, name="product"),
+    path("product/create", views.create, name="create"),
 ]
