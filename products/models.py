63d16d9dc13c2b1fe159a37e1249cfe173abec85
NickG35 <ngeorge356@gmail.com>
2025-01-28 07:35:22 -0500

    added dcategory choices and created_by field

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import Profile
 
 # Create your models here.
 class Product(models.Model):
@@ -6,7 +7,13 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images/', null=False)
     price = models.IntegerField(null=False)
     description = models.CharField(max_length=300, null=False)
-    category = models.CharField(max_length=100, null=False)
+    CATEGORY_CHOICES = [
+        ("", "Select a category"),
+        ("men's clothing", "Men's Clothing"),
+        ("women's clothing","Women's Clothing"),
+    ]
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, null=False)
+    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True, related_name='products')
 
     def __str__(self):
         return f"{self.name}"
