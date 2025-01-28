85395cea853062808a232633bc341d06d5275605
NickG35 <ngeorge356@gmail.com>
2025-01-28 07:34:46 -0500

    excluded created form field and added placeholder to category field

--- products/forms.py
+++ products/forms.py
@@ -5,3 +5,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+        exclude = ['created_by']
+        widgets = {
+            'category': forms.Select(attrs={'placeholder': 'Select a category'}),
+        }
