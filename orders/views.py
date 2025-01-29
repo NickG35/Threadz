0be371cf85083bdba0663727b44644b40a5b54ca
NickG35 <ngeorge356@gmail.com>
2025-01-29 15:48:03 -0500

    passed cart items to order page

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import CartItem
 
 # Create your views here.
 def orders(request):
-    return render(request, 'orders.html')
+    cart_items = CartItem.objects.filter(user=request.user.profile).order_by('-stow_date').all()
+    return render(request, 'orders.html', {
+        'cart_items': cart_items
+    })
