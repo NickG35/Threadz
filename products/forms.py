5fc7c233e16e3547064de77ba418c378a033ec37
NickG35 <ngeorge356@gmail.com>
2025-01-29 11:39:51 -0500

    excluded creation time from form display

--- products/forms.py
+++ products/forms.py
@@ -5,7 +5,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-        exclude = ['created_by']
+        exclude = ['created_by', 'creation_time']
         widgets = {
             'category': forms.Select(attrs={'placeholder': 'Select a category'}),
         }
