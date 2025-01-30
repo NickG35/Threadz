3505e613ac65ea993e51c15333b5351fe9337a95
NickG35 <ngeorge356@gmail.com>
2025-01-30 11:53:36 -0500

    added watchlist page/toggle urls

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("product/<int:product_id>", views.product, name="product"),
     path("product/create", views.create, name="create"),
+    path("<int:product_id>", views.toggle_watchlist, name='toggle_watch'),
+    path("watchlist/<int:profile_id>", views.watchlist_page, name='watchlist')
 ]
