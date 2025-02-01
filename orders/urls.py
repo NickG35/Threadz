9f68a52d484fd24ffb921e8dd444df5f23816387
NickG35 <ngeorge356@gmail.com>
2025-02-01 16:58:15 -0500

    set up urls for cart, checkout, and receipt

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.orders, name="orders"),
+    path("cart", views.cart, name="cart"),
+    path("checkout", views.checkout, name="checkout"),
+    path("receipt", views.receipt, name="receipt")
 ]
