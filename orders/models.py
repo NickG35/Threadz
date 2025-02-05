72fae765b347f9ec8cc59d4e3d3e626b015d7006
NickG35 <ngeorge356@gmail.com>
2025-02-05 13:17:39 -0500

    adjusted order and cartitem models for sizing and relationship

--- orders/models.py
+++ orders/models.py
@@ -11,10 +11,28 @@ class Order(models.Model):
         return f"order: {self.id}"
 
 # Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField()
+
+    def __str__(self):
+        return f"order number: {self.id}"
+    
 class CartItem(models.Model):
+    SIZE_CHOICES = [
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+        ('XXL', 'Extra Extra Large'),
+    ]
+     
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    size = models.CharField(max_length=5)
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='M')
     quantity = models.IntegerField(default=1)
     stow_date = models.DateTimeField(auto_now_add=True)
     pending_order = models.BooleanField(default=True)
