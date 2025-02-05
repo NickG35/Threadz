fe9df988461db2a1dfb19a283799fa371c1cdea1
NickG35 <ngeorge356@gmail.com>
2025-02-05 13:16:53 -0500

    added cart items to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CartItem, Order
+from .models import Order, CartItem
 
 # Register your models here.
 admin.site.register(Order)
+admin.site.register(CartItem)
