14c6f13bf2ab51dc4ad42838db1923d981791642
NickG35 <ngeorge356@gmail.com>
2025-02-05 16:45:00 -0500

    rebase solution

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from products.models import Product
 from users.models import Profile
 
+class Order(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField()
+
+    def __str__(self):
+        return f"order: {self.id}"
+
 # Create your models here.
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -10,15 +18,7 @@ class CartItem(models.Model):
     quantity = models.IntegerField(default=1)
     stow_date = models.DateTimeField(auto_now_add=True)
     pending_order = models.BooleanField(default=True)
+    order = models.ForeignKey(Order, null=True, on_delete=models.CASCADE, related_name='cart_item')
 
     def __str__(self):
         return f"{self.product}"
-
-class Order(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    cart_item = models.ManyToManyField(CartItem)
-    purchase_date = models.DateTimeField(auto_now_add=True)
-    total_price = models.IntegerField()
-
-    def __str__(self):
-        return f"order: {self.id}"
