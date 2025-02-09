f99ba46288fe05f09d60822bbb3a3a518a3d53b2
NickG35 <ngeorge356@gmail.com>
2025-02-09 16:05:49 -0500

    adjusted how profile receives the user model

--- users/models.py
+++ users/models.py
@@ -1,24 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class User(AbstractUser):
-    pass 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="profile")
 
     def __str__(self):
         return f"{self.user}"
 
 #automatically creates profile when user is created
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=get_user_model())
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 #automatically updates profile when user is updated
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=get_user_model())
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
