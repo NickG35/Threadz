9995641f33f00665769b3b863a06fcd581fd3ab0
NickG35 <ngeorge356@gmail.com>
2025-02-20 14:20:26 -0500

    added update password and user urls

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('logout/', views.logout_user, name='user_logout'),
     path('register/', views.register_user, name='register'),
     path("profile/<int:profile_id>", views.profile, name="profile"),
+    path("profile/<int:profile_id>/update_password/", views.update_password, name='update_password'),
+    path("profile/<int:profile_id>/update_user/", views.update_user, name='update_user')
 ]
