885e7d64b6a5a4721a648145dd4352ecb65d1896
NickG35 <ngeorge356@gmail.com>
2025-02-24 05:23:21 -0500

    added form place holders and classes

--- products/forms.py
+++ products/forms.py
@@ -7,5 +7,14 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
         exclude = ['created_by', 'creation_time']
         widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter product name'}),
+            'price': forms.TextInput(attrs={'placeholder': 'Enter price'}),
+            'description': forms.TextInput(attrs={'placeholder': 'Enter product description'}),
             'category': forms.Select(attrs={'placeholder': 'Select a category'}),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+            field.label = ''
