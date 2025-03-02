9dfb34b37feed606d044668de67698771281f1e5
NickG35 <ngeorge356@gmail.com>
2025-03-02 12:47:32 -0500

    added more urls for profile dashboard

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path("profile/<int:profile_id>", views.profile, name="profile"),
     path("profile/<int:profile_id>/update_password/", views.update_password, name='update_password'),
-    path("profile/<int:profile_id>/update_user/", views.update_user, name='update_user')
+    path("profile/<int:profile_id>/update_user/", views.update_user, name='update_user'),
+    path("profile/<int:profile_id>/my_clothes/", views.my_clothes, name='my_clothes'),
+    path("profile/<int:profile_id>/my_orders/", views.my_orders, name='my_orders'),
 ]
