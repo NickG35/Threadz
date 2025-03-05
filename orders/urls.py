ffc8a1f430172534b27007c3b7febd9f27674124
NickG35 <ngeorge356@gmail.com>
2025-03-05 07:41:49 -0500

    implemeneted order detail url

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("cart", views.cart, name="cart"),
     path("checkout", views.checkout, name="checkout"),
-    path("receipt", views.receipt, name="receipt")
+    path("receipt", views.receipt, name="receipt"),
+    path("order_details/<int:order_id>", views.order_details, name="order_details")
 ]
