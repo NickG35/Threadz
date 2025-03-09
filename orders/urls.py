3088f7e1c9027e1cdd8e33bd24addab588d11a25
NickG35 <ngeorge356@gmail.com>
2025-03-09 14:52:45 -0400

    created delete item url

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("cart", views.cart, name="cart"),
+    path("delete_item/<int:product_id>", views.delete_item, name="delete_item"),
     path("checkout", views.checkout, name="checkout"),
     path("receipt", views.receipt, name="receipt"),
     path("order_details/<int:order_id>", views.order_details, name="order_details")
