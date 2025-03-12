6670be1130cc759983622fd0f51ae7f636678909
NickG35 <ngeorge356@gmail.com>
2025-03-12 11:35:44 -0400

    added remove_item url

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("product/<int:product_id>", views.product, name="product"),
     path("product/create", views.create, name="create"),
     path("<int:product_id>", views.toggle_watchlist, name='toggle_watch'),
+    path("remove_item/<int:product_id>", views.remove_item, name='remove_item'),
     path("watchlist/<int:profile_id>", views.watchlist_page, name='watchlist')
 ]
