3c2f872fe292d5adbd25205344cb68507a550093
NickG35 <ngeorge356@gmail.com>
2025-03-15 16:51:24 -0400

    added id to receipt path for order id specific details to be displayed

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("cart", views.cart, name="cart"),
     path("delete_item/<int:product_id>", views.delete_item, name="delete_item"),
     path("checkout", views.checkout, name="checkout"),
-    path("receipt", views.receipt, name="receipt"),
+    path("receipt/<int:purchase_id>", views.receipt, name="receipt"),
     path("order_details/<int:order_id>", views.order_details, name="order_details")
 ]
