b4ebdd72192bbf3a4f74bb5ee03c2bca384977ae
NickG35 <ngeorge356@gmail.com>
2025-04-02 07:17:42 -0400

    added updated_time field for cart_item for ordering

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from products.models import Product
 from users.models import Profile
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -34,8 +35,15 @@ class CartItem(models.Model):
     size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='M')
     quantity = models.IntegerField(default=1)
     stow_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
     pending_order = models.BooleanField(default=True)
     order = models.ForeignKey(Order, null=True, on_delete=models.CASCADE, related_name='cart_item')
 
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.updated_time = now()  # Update timestamp when saving
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.product}"
+    
