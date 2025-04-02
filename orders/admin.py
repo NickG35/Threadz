befe242c14dbf43e13e86789823a21d316268bed
NickG35 <ngeorge356@gmail.com>
2025-04-02 07:19:04 -0400

    adjusted database display for cart items

--- orders/admin.py
+++ orders/admin.py
@@ -3,4 +3,14 @@ from .models import Order, CartItem
 
 # Register your models here.
 admin.site.register(Order)
-admin.site.register(CartItem)
+
+class CartItemAdmin(admin.ModelAdmin):
+    # Specify the fields to display in the list view
+    list_display = ('product', 'user', 'size', 'quantity', 'stow_date', 'updated_time', 'pending_order')
+    
+    # Optionally add search and filters
+    search_fields = ('product__name', 'user__username')
+    list_filter = ('size', 'pending_order')
+
+# Register the model with the custom admin
+admin.site.register(CartItem, CartItemAdmin)
