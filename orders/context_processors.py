3601ec06353e06bfcd88d274da1a8cac07ebdd55
NickG35 <ngeorge356@gmail.com>
2025-04-02 07:18:26 -0400

    adjusted cart_count and ordering of items for base display

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -3,9 +3,9 @@ from django.db.models import Sum, F
 
 def cart_context(request):
     if request.user.is_authenticated:
-        cart_items = CartItem.objects.filter(user=request.user.profile, pending_order=True)
+        cart_items = CartItem.objects.filter(user=request.user.profile, pending_order=True).order_by('-updated_time').all()
         cart_total = cart_items.aggregate(total=Sum(F('product__price') * F('quantity')))['total'] or 0
-        cart_count = cart_items.count()
+        cart_count = cart_items.aggregate(total_count=Sum('quantity'))['total_count'] or 0
     else:
         cart_items = []
         cart_total = 0
